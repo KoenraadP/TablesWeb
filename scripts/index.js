5d085a2fc82f8bf1962c410ecbb6b55cf3eed15d
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2024-06-11 14:47:03 +0200

    Add greeting

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// add names to select
+
 const userSelect = document.getElementById("user-select");
 
 fetch("users.json")
@@ -7,9 +9,20 @@ fetch("users.json")
     .then(users => {
         users.forEach(user => {
            const option = document.createElement("option");
-           option.value = user.id;
+           option.value = user.name;
            option.textContent = user.name;
            userSelect.appendChild(option);
         });
     })
     .catch(error => console.error("Error loading user data", error));
+
+// form event
+// save name to session and proceed to next page
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    sessionStorage.setItem("user",userSelect.value);
+    location.assign("tables.html");
+})
---
+++ scripts/tables.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// greeting
+
+const user = sessionStorage.getItem("user");
+document.getElementById("greeting").innerText += " " + user + "!";
+
+// table numbers 2 through 9
+
+const tableNumbers = [2,3,4,5,6,7,8,9];
+
+
+
